47b7454dcb265aa7e589a0e5e40cc6235367cb01
Sebastian <surregograciano@gmail.com>
2025-04-22 11:19:55 -0500

    first-commit-java

---
+++ Proyecto_1/src/Variables_01.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Sebastian"; // class String
+        int value = 100; // int
+        double value2 = 10.5; // double
+        char letter = 'S'; // char entre comillas simples
+        boolean isEstudent = true; // true or false
+
+        System.out.println(name);
+        System.out.println(value);
+        System.out.println(value2);
+        System.out.println(letter);
+        System.out.println(isEstudent);
+    }
+}
