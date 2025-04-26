254b82c86fdb22c5ea5a4f2b0eb5f38fdfa4e57d
Sebastian <surregograciano@gmail.com>
2025-04-26 13:12:07 -0500

    Auto stash before merge of "main" and "origin/main"

--- Proyecto_1/src/Joptionpane_05.java
+++ Proyecto_1/src/Joptionpane_05.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Joptionpane_05 {
+    public static void main(String[] args) {
+
+        // Ingreso de datos con una forma grafica
+
+        String name;
+        int age;
+
+        // Ingreso de datos
+
+        name = JOptionPane.showInputDialog(null, "Ingrese su nombre...");
+        String age2 = JOptionPane.showInputDialog(null, "Ingrese su edad...");
+        age = Integer.parseInt(age2);
+
+
+        // mostramos datos
+        JOptionPane.showMessageDialog(null, "name: " + name + "\n" + "age: " + age);
+    }
 }
--- Proyecto_1/src/VarJava_03.java
+++ Proyecto_1/src/VarJava_03.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class VarJava_03 {
+    public static void main(String[] args) {
+
+        var name = "Sebastian";
+        var number = 18;
+        double number2 = 10.50;
+
+        System.out.println(name);
+    }
 }
