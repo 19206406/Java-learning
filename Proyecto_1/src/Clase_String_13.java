152893cb21e83764da613a0cac013bd4aea6b025
Sebastian <surrego@nexos-software.com.co>
2025-04-29 17:30:28 -0500

    metodos-strings

---
+++ Proyecto_1/src/Clase_String_13.java
@@ -0,0 +1,54 @@
+package PACKAGE_NAME;
+
+import java.util.Scanner;
+
+public class Clase_String_13 {
+    public static void main(String[] args) {
+        String word1, word2;
+        Scanner input = new Scanner(System.in);
+
+        // ingreso de datos
+        System.out.println("Ingrese primera palabra...");
+        word1 = input.next();
+        System.out.println("Ingrese segunda palabra...");
+        word2 = input.next();
+
+        // comparar texto
+        if (word1.equals(word2)) {
+            System.out.println(word1 + " es igual a: " + word2);
+        } else {
+            System.out.println(word1 + " no es igual a: " + word2);
+        }
+
+        // compara textos sin tomar en cuenta mayusculas
+        if (word1.equalsIgnoreCase(word2)) {
+            System.out.println(word1 + " es igual a: " + word2 + "\n" +
+                    " sin tomar en cuenta las mayusculas");
+        } else {
+            System.out.println(word1 + " no es igual a: " + word2 + "\n" +
+                    " sin tomar en cuenta las mayusculas");
+        }
+
+        // compara cantidad de caracteres
+        if (word1.compareTo(word2) == 0) {
+            System.out.println(word1 + " tiene las mismas letras que: " + word2);
+        } else {
+            if (word1.compareTo(word2) > 0) {
+                System.out.println(word1 + " tiene mas letras que: " + word2);
+            } else {
+                System.out.println(word1 + " tiene menos letras que: " + word2);
+            }
+        }
+
+        // saber caracter de una posición
+        char character = word1.charAt(0);
+        System.out.println("posición " + word1 + ": " + character);
+
+        // Cantidad de caracteres de una palabra o palabra
+        System.out.println("Cantidad de caracteres " + word1 + ": " + word1.length());
+        System.out.println("Cantidad de caracteres " + word2 + ": " + word2.length());
+
+        // identificar varios caractes en un texto o palabra
+        System.out.println(word1.substring(1, 3));
+    }
+}
