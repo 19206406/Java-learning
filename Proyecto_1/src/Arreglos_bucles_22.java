dd2a333bca2cbcad7e6abf89cba8684db214212a
Sebastian <surrego@nexos-software.com.co>
2025-05-13 17:29:37 -0500

    arrays-parte-1

---
+++ Proyecto_1/src/Arreglos_bucles_22.java
@@ -0,0 +1,18 @@
+package PACKAGE_NAME;
+
+public class Arreglos_bucles_22 {
+    public static void main(String[] args) {
+        String[] productos = {"", "Martillo", "Destornillador", "Taladro", "Llave inglesa", "Pinzas"};
+
+        /*for (int f = 0; f < productos.length; f++) {
+            System.out.println("Producto: " + f + " " + productos[f]);
+        }*/
+
+        // recorrer array con bucle while
+        int f = 0;
+        while (f < productos.length) {
+            System.out.println("Producto: " + productos[f]);
+            f++;
+        }
+    }
+}
